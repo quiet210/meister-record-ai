94b0b1c84c142d02a151ccf557ced43753314a86
quiet210 <quiet02@hanmail.net>
2026-06-24 13:56:30 +0900

    Add files via upload

---
+++ app/admin/curriculum/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/AppShell";
+import { CurriculumManager } from "@/components/CurriculumManager";
+
+export default function AdminCurriculumPage() {
+  return (
+    <AppShell>
+      <CurriculumManager />
+    </AppShell>
+  );
+}
