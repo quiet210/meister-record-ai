7fe68b62b713064ef9b3d813de70f0cdd6926b17
quiet210 <quiet02@hanmail.net>
2026-06-26 10:49:01 +0900

    Add files via upload

---
+++ app/bulk-subject-comment/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/AppShell";
+import { BulkSubjectCommentComposer } from "@/components/BulkSubjectCommentComposer";
+
+export default function BulkSubjectCommentPage() {
+  return (
+    <AppShell>
+      <BulkSubjectCommentComposer />
+    </AppShell>
+  );
+}
