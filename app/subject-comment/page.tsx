7d0aa80f1db2f10171b52985d39bd04dddc0780c
quiet210 <quiet02@hanmail.net>
2026-07-01 11:34:23 +0900

    Add files via upload

--- app/subject-comment/page.tsx
+++ app/subject-comment/page.tsx
@@ -1,10 +1,14 @@
 import { AppShell } from "@/components/AppShell";
+import { CommentComposerTabs } from "@/components/CommentComposerTabs";
 import { RecordComposer } from "@/components/RecordComposer";
 
 export default function SubjectCommentPage() {
   return (
     <AppShell>
-      <RecordComposer mode="subject" />
+      <div className="min-w-0 space-y-4">
+        <CommentComposerTabs section="subject" activeTab="single" />
+        <RecordComposer mode="subject" />
+      </div>
     </AppShell>
   );
 }
