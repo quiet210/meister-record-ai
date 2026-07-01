ab768f3dd4a647e01257a885ac65da4f10ae74a7
quiet210 <quiet02@hanmail.net>
2026-07-01 11:34:52 +0900

    Add files via upload

--- app/bulk-subject-comment/page.tsx
+++ app/bulk-subject-comment/page.tsx
@@ -1,10 +1,14 @@
 import { AppShell } from "@/components/AppShell";
 import { BulkSubjectCommentComposer } from "@/components/BulkSubjectCommentComposer";
+import { CommentComposerTabs } from "@/components/CommentComposerTabs";
 
 export default function BulkSubjectCommentPage() {
   return (
     <AppShell>
-      <BulkSubjectCommentComposer />
+      <div className="min-w-0 space-y-4">
+        <CommentComposerTabs section="subject" activeTab="bulk" />
+        <BulkSubjectCommentComposer />
+      </div>
     </AppShell>
   );
 }
