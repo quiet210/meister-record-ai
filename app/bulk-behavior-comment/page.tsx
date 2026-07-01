e3c04d40fe1955556c2e41903fe6613465c79695
quiet210 <quiet02@hanmail.net>
2026-07-01 11:36:01 +0900

    Add files via upload

--- app/bulk-behavior-comment/page.tsx
+++ app/bulk-behavior-comment/page.tsx
@@ -1,10 +1,14 @@
 import { AppShell } from "@/components/AppShell";
 import { BulkBehaviorCommentComposer } from "@/components/BulkBehaviorCommentComposer";
+import { CommentComposerTabs } from "@/components/CommentComposerTabs";
 
 export default function BulkBehaviorCommentPage() {
   return (
     <AppShell>
-      <BulkBehaviorCommentComposer />
+      <div className="min-w-0 space-y-4">
+        <CommentComposerTabs section="behavior" activeTab="bulk" />
+        <BulkBehaviorCommentComposer />
+      </div>
     </AppShell>
   );
 }
