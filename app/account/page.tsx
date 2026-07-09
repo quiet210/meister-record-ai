11f11def3d21cfc8d2de92f89c09333028ee9020
quiet210 <quiet02@hanmail.net>
2026-07-09 09:45:34 +0900

    Add files via upload

---
+++ app/account/page.tsx
@@ -0,0 +1,10 @@
+import { AccountManager } from "@/components/AccountManager";
+import { AppShell } from "@/components/AppShell";
+
+export default function AccountPage() {
+  return (
+    <AppShell>
+      <AccountManager />
+    </AppShell>
+  );
+}
